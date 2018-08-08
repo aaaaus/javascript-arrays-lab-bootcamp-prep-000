7252e8cbaf3141604b3a228bd0b3d14178c4a88f
Austin Luft <ausluft@gmail.com>
2018-08-08 02:35:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 // Add your functions and code here
+
 function destructivelyAppendKitten (name) {
   return kittens.push(name)
 }
+
+function destrutivelyPrependKitten (name) {
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten (name) {
+  return kittens.pop()
+}
+
